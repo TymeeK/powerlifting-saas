4b72051c6413941fb9ca3ead6b95f69115a43268
Tymee Kong <tymeekong562@gmail.com>
2025-11-02 19:33:02 -0800

    Removed lazy initializing

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -18,78 +18,79 @@ const firebaseConfig = {
 const app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-// Initialize Firebase services
-// Auth requires browser environment - use lazy initialization
-let _authInstance: Auth | null = null;
+// // Initialize Firebase services
+// // Auth requires browser environment - use lazy initialization
+// let _authInstance: Auth | null = null;
 
-// Get auth instance - lazy initialization only in browser
-function getAuthInstance(): Auth {
-  if (typeof window === 'undefined') {
-    // This should never be called during SSR since client components
-    // don't execute during SSR. But we handle it just in case.
-    throw new Error(
-      'Firebase Auth can only be accessed in the browser. Make sure your component is a client component.'
-    );
-  }
-  if (!_authInstance) {
-    _authInstance = getAuth(app);
-  }
-  return _authInstance;
-}
+// // Get auth instance - lazy initialization only in browser
+// function getAuthInstance(): Auth {
+//   if (typeof window === 'undefined') {
+//     // This should never be called during SSR since client components
+//     // don't execute during SSR. But we handle it just in case.
+//     throw new Error(
+//       'Firebase Auth can only be accessed in the browser. Make sure your component is a client component.'
+//     );
+//   }
+//   if (!_authInstance) {
+//     _authInstance = getAuth(app);
+//   }
+//   return _authInstance;
+// }
 
-// Export auth as a getter property that initializes on first access
-// This ensures getAuth() is never called during SSR/build
-export const auth = new Proxy({} as Auth, {
-  get(_target, prop) {
-    const instance = getAuthInstance();
-    const value = instance[prop as keyof Auth];
-    if (typeof value === 'function') {
-      return value.bind(instance);
-    }
-    return value;
-  },
-  set(_target, prop, value) {
-    // Allow Firebase to set internal properties on the actual instance
-    try {
-      const instance = getAuthInstance();
-      (instance as any)[prop] = value;
-      return true;
-    } catch {
-      return false;
-    }
-  },
-  getOwnPropertyDescriptor(_target, prop) {
-    const instance = getAuthInstance();
-    return Object.getOwnPropertyDescriptor(instance, prop as string);
-  },
-  defineProperty(_target, prop, descriptor) {
-    // Allow Firebase to define properties on the actual instance
-    try {
-      const instance = getAuthInstance();
-      Object.defineProperty(instance, prop as string, descriptor);
-      return true;
-    } catch {
-      return false;
-    }
-  },
-  has(_target, prop) {
-    try {
-      const instance = getAuthInstance();
-      return prop in instance;
-    } catch {
-      return false;
-    }
-  },
-  ownKeys(_target) {
-    try {
-      const instance = getAuthInstance();
-      return Object.keys(instance);
-    } catch {
-      return [];
-    }
-  },
-});
+// // Export auth as a getter property that initializes on first access
+// // This ensures getAuth() is never called during SSR/build
+// export const auth = new Proxy({} as Auth, {
+//   get(_target, prop) {
+//     const instance = getAuthInstance();
+//     const value = instance[prop as keyof Auth];
+//     if (typeof value === 'function') {
+//       return value.bind(instance);
+//     }
+//     return value;
+//   },
+//   set(_target, prop, value) {
+//     // Allow Firebase to set internal properties on the actual instance
+//     try {
+//       const instance = getAuthInstance();
+//       (instance as any)[prop] = value;
+//       return true;
+//     } catch {
+//       return false;
+//     }
+//   },
+//   getOwnPropertyDescriptor(_target, prop) {
+//     const instance = getAuthInstance();
+//     return Object.getOwnPropertyDescriptor(instance, prop as string);
+//   },
+//   defineProperty(_target, prop, descriptor) {
+//     // Allow Firebase to define properties on the actual instance
+//     try {
+//       const instance = getAuthInstance();
+//       Object.defineProperty(instance, prop as string, descriptor);
+//       return true;
+//     } catch {
+//       return false;
+//     }
+//   },
+//   has(_target, prop) {
+//     try {
+//       const instance = getAuthInstance();
+//       return prop in instance;
+//     } catch {
+//       return false;
+//     }
+//   },
+//   ownKeys(_target) {
+//     try {
+//       const instance = getAuthInstance();
+//       return Object.keys(instance);
+//     } catch {
+//       return [];
+//     }
+//   },
+// });
 
+export const auth: Auth = getAuth(app);
 export const db: Firestore = getFirestore(app);
 export const storage: FirebaseStorage = getStorage(app);
 
